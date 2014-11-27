7e57a18486dadca33510c60cf8ed6b35f51171bd
Nhan Tran <nhan4545@gmail.com>
2014-11-27 15:01:03 -0800

    Troubleshooting continued

--- app/src/main/java/com/example/nhan/myapplication/LocationManager.java
+++ app/src/main/java/com/example/nhan/myapplication/LocationManager.java
@@ -1,17 +1,24 @@
 package com.example.nhan.myapplication;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.nhan.myapplication.SQLite.DAL;
+import com.example.nhan.myapplication.SQLite.DrivingDataContract;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -22,11 +29,11 @@ public class LocationManager implements GoogleApiClient.ConnectionCallbacks,
     Context mContext;
     //LocationClient mLocationClient;
     GoogleApiClient mClient;
-    Boolean mConnected;
+    public Boolean mConnected;
 
     public LocationManager(Context context) {
         mContext = context;
-        mConnected = false;
+        //mConnected = false;
         //mLocationClient = new LocationClient(mContext, this, this);
         // Connect the client.
         //mLocationClient.connect();
@@ -37,15 +44,24 @@ public class LocationManager implements GoogleApiClient.ConnectionCallbacks,
                 .addOnConnectionFailedListener(this)
                 .build();
 
-        mClient.connect();
+        ///mClient.connect();
     }
 
     public Location getLocation(){
         Location currentLocation = null;
-        FusedLocationProviderApi fusedProvider = LocationServices.FusedLocationApi;
-        if (mConnected){
-            currentLocation = fusedProvider.getLastLocation(mClient);
+        //FusedLocationProviderApi fusedProvider = LocationServices.FusedLocationApi;
+        ConnectionResult result =  mClient.blockingConnect();
+        if (result.isSuccess()) {
+            mConnected = true;
+            if (mClient.isConnected()){
+                currentLocation = LocationServices.FusedLocationApi.getLastLocation(mClient);
+                mClient.disconnect();
+            }
         }
+        else {
+            mConnected = false;
+        }
+
 
         return currentLocation;
     }
@@ -53,6 +69,33 @@ public class LocationManager implements GoogleApiClient.ConnectionCallbacks,
     @Override
     public void onConnected(Bundle bundle) {
         mConnected = true;
+        DAL dal = new DAL(this);
+        int confidence = activity.getConfidence();
+        int activityType = activity.getType();
+        String activityName = getNameFromType(activityType);
+
+        Location currentLocation = mLocationManager.getLocation();
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        // Create a new map of values, where column names are the keys
+
+        ContentValues values = new ContentValues();
+        //values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_ENTRY_ID, "testId");
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_ACTIVITY_STATUS, activityName);
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_CREATED_DATE, dateFormat.format(date));
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_SPEED, currentLocation.getSpeed());
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_LONGITUDE, currentLocation.getLongitude());
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_LATITUDE, currentLocation.getLatitude());
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_LOCATION_TIME, currentLocation.getTime());
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_BEARING, currentLocation.getBearing());
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_USER_ID, "testUserId");
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_SYNCED, 0);
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_HAS_LOCATION, (currentLocation != null));
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_CONFIDENCE, confidence);
+        values.put(DrivingDataContract.DrivingEntry.COLUMN_NAME_ACCURACY, currentLocation.getAccuracy());
+
+        dal.WriteLog(values);
     }
 
     @Override
